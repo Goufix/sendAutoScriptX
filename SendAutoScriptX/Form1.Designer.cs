84e989c91b8f23c7abe8fed503074f32dcd7e373
Alifer <alifer.17.11@gmail.com>
2021-08-28 20:33:51 -0300

    First commit (fake fork)

---
+++ SendAutoScriptX/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SendAutoScriptX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtFrase = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(-3, -2);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(358, 56);
+            this.txtFrase.TabIndex = 0;
+            this.txtFrase.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 53);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFrase);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(416, 92);
+            this.Name = "Form1";
+            this.Text = "SendAutoScriptX v1.0.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtFrase;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SendAutoScriptX/Form1.cs
@@ -0,0 +1,327 @@
+﻿using FMUtils.KeyboardHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SendAutoScriptX
+{
+    public partial class Form1 : Form
+    {
+        bool topMost = false;
+        Hook kb = new Hook("kb");
+        int contagem = 0;
+        List<string> frases = new List<string>();
+        string arquivo = "";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            kb.KeyDownEvent += kbhooks;
+            MessageBox.Show("Para deixar o programa sem borda, no canto da tela e sempre acima dos programas aperte F2\nPara avançar uma linha aperte F4\nPara retroceder uma linha aperte F3\n=============================================\nPara enviar o conteúdo da caixa de texto aperte F7");
+        }
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        #region MÉTODO CORREÇÃO
+        private string CorrecaoLinha(string fraseErrada)
+        {
+            if (fraseErrada.IndexOf("{)}") != -1 || fraseErrada.IndexOf("{(}") != -1 || fraseErrada.IndexOf("{+}") != -1 || fraseErrada.IndexOf("{~}") != -1 ||
+                fraseErrada.IndexOf("{^}") != -1 || fraseErrada.IndexOf("{%}") != -1)
+            {
+                return fraseErrada;
+            }
+            int inicio = 1;
+            int ColcheteAbre, ColcheteFecha, Circuflexo, Porcentagem, Til, Mais = 0;
+            string frase = fraseErrada;
+            if (frase.IndexOf("(") != -1)
+            {
+                ColcheteAbre = 0;
+            }
+            else
+            {
+                ColcheteAbre = 1;
+            }
+            if (frase.IndexOf(")") != -1)
+            {
+                ColcheteFecha = 0;
+            }
+            else
+            {
+                ColcheteFecha = 1;
+            }
+            if (frase.IndexOf("^") != -1)
+            {
+                Circuflexo = 0;
+            }
+            else
+            {
+                Circuflexo = 1;
+            }
+            if (frase.IndexOf("%") != -1)
+            {
+                Porcentagem = 0;
+            }
+            else
+            {
+                Porcentagem = 1;
+            }
+            if (frase.IndexOf("~") != -1)
+            {
+                Til = 0;
+            }
+            else
+            {
+                Til = 1;
+            }
+            if (frase.IndexOf("+") != -1)
+            {
+                Mais = 0;
+            }
+            else
+            {
+                Mais = 1;
+            }
+            string frasePronta = "";
+            while (ColcheteAbre == 0)
+            {
+                if (frase.IndexOf("(") != -1 && frase.IndexOf("{(}") == -1)
+                {
+                    string[] FraseSplit = frase.Split('(');
+                    foreach (string x in FraseSplit)
+                    {
+                        if (inicio == 1)
+                        {
+                            frasePronta = frasePronta + x;
+                            inicio = 0;
+                        }
+                        else
+                        {
+                            frasePronta = frasePronta + "{(}" + x;
+                        }
+                    }
+                    ColcheteAbre = 1; frase = frasePronta; frasePronta = ""; inicio = 1;
+                }
+            }
+            while (ColcheteFecha == 0)
+            {
+                if (frase.IndexOf(")") != -1 && frase.IndexOf("{)}") == -1)
+                {
+                    string[] FraseSplit = frase.Split(')');
+                    //string frasePronta = "";
+                    foreach (string x in FraseSplit)
+                    {
+                        if (inicio == 1)
+                        {
+                            frasePronta += x;
+                            inicio = 0;
+                        }
+                        else
+                        {
+                            frasePronta = frasePronta + "{)}" + x;
+                        }
+                    }
+                    ColcheteFecha = 1;
+                    frase = frasePronta;
+                    frasePronta = "";
+                    inicio = 1;
+                }
+            }
+            while (Circuflexo == 0)
+            {
+                if (frase.IndexOf("^") != -1 && frase.IndexOf("{^}") == -1)
+                {
+                    string[] FraseSplit = frase.Split('^');
+                    //string frasePronta = "";
+                    foreach (string x in FraseSplit)
+                    {
+                        if (inicio == 1)
+                        {
+                            frasePronta += x;
+                            inicio = 0;
+                        }
+                        else
+                        {
+                            frasePronta = frasePronta + "{^}" + x;
+                        }
+                        Circuflexo = 1;
+                        inicio = 1;
+                        frase = frasePronta;
+                    }
+                    Circuflexo = 1;
+                    frase = frasePronta;
+                    frasePronta = "";
+                    inicio = 1;
+                }
+                else { Circuflexo = 1; }
+            }
+            while (Porcentagem == 0)
+            {
+                if (frase.IndexOf("%") != -1 && frase.IndexOf("{%}") == -1)
+                {
+                    string[] FraseSplit = frase.Split('%');
+                    //string frasePronta = "";
+                    foreach (string x in FraseSplit)
+                    {
+                        if (inicio == 1)
+                        {
+                            frasePronta = frasePronta + x;
+                            inicio = 0;
+                        }
+                        else
+                        {
+                            frasePronta = frasePronta + "{%}" + x;
+                        }
+                    }
+                    Porcentagem = 1;
+                    frase = frasePronta;
+                    frasePronta = "";
+                    inicio = 1;
+                }
+                else { Porcentagem = 1; }
+            }
+            while (Til == 0)
+            {
+                if (frase.IndexOf("~") != -1 && frase.IndexOf("{~}") == -1)
+                {
+                    string[] FraseSplit = frase.Split('~');
+                    //string frasePronta = "";
+                    foreach (string x in FraseSplit)
+                    {
+                        if (inicio == 1)
+                        {
+                            frasePronta += x;
+                            inicio = 0;
+                        }
+                        else
+                        {
+                            frasePronta = frasePronta + "{~}" + x;
+                        }
+                    }
+                    Til = 1;
+                    frase = frasePronta;
+                    frasePronta = "";
+                    inicio = 1;
+                }
+                else { Til = 1; }
+            }
+            while (Mais == 0)
+            {
+                if (frase.IndexOf("+") != -1 && frase.IndexOf("{+}") == -1)
+                {
+                    string[] FraseSplit = frase.Split('+');
+                    //string frasePronta = "";
+                    foreach (string x in FraseSplit)
+                    {
+                        if (inicio == 1)
+                        {
+                            frasePronta += x;
+                            inicio = 0;
+                        }
+                        else
+                        {
+                            frasePronta = frasePronta + "{+}" + x;
+                        }
+                    }
+                    Mais = 1;
+                    frase = frasePronta;
+                    frasePronta = "";
+                    inicio = 1;
+                }
+                else { Mais = 1; }
+            }
+            return frase;
+        }
+        #endregion
+
+        private void kbhooks(KeyboardHookEventArgs e)
+        {
+            if (e.Key == Keys.F2)
+            {
+                if (!topMost)
+                {
+                    this.Location = new Point(0, 400);
+                    this.TopMost = true;
+                    this.FormBorderStyle = FormBorderStyle.None;
+                    topMost = true;
+                }
+                else
+                {
+                    this.Location = new Point(200, 400);
+                    this.TopMost = false;
+                    this.FormBorderStyle = FormBorderStyle.Sizable;
+                    topMost = false;
+                }
+            }
+            if (e.Key == Keys.F3)
+            {
+                int contagemDefault = contagem;
+                try {
+                contagem -= 1;
+                txtFrase.Text = frases[contagem]; }catch { contagem = contagemDefault; }
+            }
+            if (e.Key == Keys.F4)
+            {
+                int contagemDefault = contagem;
+                try
+                {
+                    contagem += 1;
+                    txtFrase.Text = frases[contagem];
+                }
+                catch { contagem = contagemDefault; }
+            }
+            if (e.Key == Keys.F7)
+            {
+                int contagemDefault = contagem;
+                try {
+                    if (Control.IsKeyLocked(Keys.CapsLock))
+                    {
+                        const int KEYEVENTF_EXTENDEDKEY = 0x1;
+                        const int KEYEVENTF_KEYUP = 0x2;
+                        keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                        keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+                        (UIntPtr)0);
+                    }
+                    string fraseAEnviar = CorrecaoLinha(txtFrase.Text);
+                    SendKeys.Send(fraseAEnviar);
+                    SendKeys.SendWait("+{Enter}");
+                    contagem += 1;
+                    txtFrase.Text = frases[contagem];
+                }
+                catch { contagem = contagemDefault; }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frases.Clear();
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Abrir Script";
+            file.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                arquivo = file.FileName;
+                StreamReader sr = new StreamReader(file.FileName, Encoding.Default);
+                while (!sr.EndOfStream)
+                {
+                    string linha = sr.ReadLine();
+                    if(linha != "")
+                        frases.Add(linha);
+                }
+                txtFrase.Text = frases[0];
+            }
+            
+        }
+    }
+}
